be2af012687fe404854016a804ef77703bd1f8c2
domenhribernik <domen.hribernik4@gmail.com>
2024-05-08 20:43:34 +0200

    Fixed brute force

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -8,98 +8,85 @@
 using namespace std;
 using namespace chrono;
 
-int scan(const string &bits, int m)
-{
-  int sum = 0;
-  for (int i = m - 1; i < bits.size(); i += m)
-  {
-    if (bits[i] == '1')
-    {
-      sum++;
+int scan(const string &bits, int m) {
+    int sum = 0;
+    for (int i = m - 1; i < bits.size(); i += m) {
+        if (bits[i] == '1') {
+            sum++;
+        }
     }
-  }
-  return sum;
+    return sum;
 }
 
-pair<int, vector<int>> insertFruit(string bits, int k, int m, int index = 0, int insertions = 0, vector<int> positions = {})
-{
-  // Base case: if no more apples can be inserted or we've reached the end of the string
-  if (insertions == k || index > bits.size())
-  {
-    return make_pair(scan(bits, m), positions);
-  }
+pair<int, vector<int>> insertFruit(string bits, int k, int m, int index = 0, int insertions = 0, vector<int> positions = {}) {
+    if (k == 0 || index >= bits.size()) {
+        // Scan the sequence as it is and return the number of apples picked
+        return make_pair(scan(bits, m), positions);
+    }
 
-  // Try inserting an apple at the current position
-  string withApple = bits.substr(0, index) + '1' + bits.substr(index);
-  vector<int> withApplePositions = positions;
-  withApplePositions.push_back(index + 1);
-  pair<int, vector<int>> withAppleScore = insertFruit(withApple, k, m, index + 1, insertions + 1, withApplePositions);
+    // Option 1: Do not insert an apple at this position
+    auto resultWithoutInsertion = insertFruit(bits, k, m, index + 1, insertions, positions);
 
-  // Try not inserting an apple at the current position
-  pair<int, vector<int>> withoutAppleScore = insertFruit(bits, k, m, index + 1, insertions, positions);
+    // Option 2: Try inserting an apple at this position if we have any left
+    pair<int, vector<int>> resultWithInsertion = {INT_MAX, {}};
+    if (k > 0) {
+        string newBits = bits.substr(0, index) + "1" + bits.substr(index);
+        vector<int> newPositions = positions;
+        newPositions.push_back(index + 1); // Use 1-based index for output
+        resultWithInsertion = insertFruit(newBits, k - 1, m, index + 1, insertions + 1, newPositions);
+    }
 
-  // Return the pair with the minimum score
-  if (withAppleScore.first < withoutAppleScore.first)
-  {
-    return withAppleScore;
-  }
-  else
-  {
-    return withoutAppleScore;
-  }
+    // Return the better option
+    if (resultWithoutInsertion.first <= resultWithInsertion.first) {
+        return resultWithoutInsertion;
+    } else {
+        return resultWithInsertion;
+    }
 }
 
-int main(int argc, char *argv[])
-{
-  auto start = high_resolution_clock::now();
+int main(int argc, char *argv[]) {
+    auto start = high_resolution_clock::now();
 
-  if (argc != 2)
-  {
-    cout << "Usage: " << argv[0] << " <filename>" << endl;
-    return 1;
-  }
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
 
-  string filename = argv[1];
-  ifstream file(filename);
-  if (!file.is_open())
-  {
-    cout << "Failed to open the file." << endl;
-    return 1;
-  }
+    string filename = argv[1];
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Failed to open the file." << endl;
+        return 1;
+    }
 
-  string line;
-  int n, k, m;
-  string fruit;
-  if (getline(file, line))
-  {
-    stringstream ss(line);
-    ss >> n >> k >> m;
-  }
-  else
-  {
-    cout << "Wrong format" << endl;
-    return 1;
-  }
+    string line;
+    int n, k, m;
+    string fruit;
+    if (getline(file, line)) {
+        stringstream ss(line);
+        ss >> n >> k >> m;
+    } else {
+        cout << "Wrong format" << endl;
+        return 1;
+    }
 
-  if (getline(file, line))
-  {
-    stringstream ss(line);
-    ss >> fruit;
-  }
+    if (getline(file, line)) {
+        stringstream ss(line);
+        ss >> fruit;
+    }
 
-  file.close();
+    file.close();
 
-  pair<int, vector<int>> result = insertFruit(fruit, k, m);
-  cout << result.first << endl;
-  cout << result.second.size() << " ";
-  for (int pos : result.second)
-  {
-    cout << pos << " ";
-  }
-  cout << endl;
+    pair<int, vector<int>> result = insertFruit(fruit, k, m);
+    cout << result.first << endl;
+    cout << result.second.size() << " ";
+    for (int pos : result.second) {
+        cout << pos << " ";
+    }
+    cout << endl;
 
-  auto end = high_resolution_clock::now();
-  duration<double> duration_seconds = end - start;
-  cout << "Elapsed time: " << duration_seconds.count() << " seconds" << endl;
-  return 0;
+    auto end = high_resolution_clock::now();
+    duration<double> duration_seconds = end - start;
+    cout << "Elapsed time: " << duration_seconds.count() << " seconds" << endl;
+    return 0;
 }
