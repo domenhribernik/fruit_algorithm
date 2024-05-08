3b512b050196c7a630d07c3a12a3d95f57586453
zankazar107 <kazar.zan@gmail.com>
2024-05-08 16:03:00 +0200

    Ne crasha več, ene primere reši prav, vseeno ni optimalno :(

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ struct Node {
 
 int pick_apples(int m, vector<bool> fruits) {
   int appleCount = 0;
-  for (size_t i = 0; i < fruits.size(); i += m) {
+  for (size_t i = m-1; i < fruits.size(); i += m) {
     if (fruits[i] == 1) {
       appleCount++;
     }
@@ -25,10 +25,22 @@ int pick_apples(int m, vector<bool> fruits) {
   return appleCount;
 }
 
+int pick_apples_value(int m, vector<bool> fruits, int index) {
+  int appleCount = 0;
+  fruits.insert(fruits.begin() + index, 1);
+  for (size_t i = m-1; i < fruits.size(); i += m) {
+    if (fruits[i] == 1) {
+      appleCount++;
+    }
+  }
+
+  return appleCount;
+}
+
 pair<vector<int>, int> solve(int n, int k, int m, vector<bool>& fruits) {
   vector<int> insertedIndices;
   int appleCount = 0;
-
+  bool stop = false;
   bool check = true;
   int check_value = fruits[0];   
   for (int i = 1; i < n; i++) {
@@ -38,51 +50,34 @@ pair<vector<int>, int> solve(int n, int k, int m, vector<bool>& fruits) {
     }
   }
 
-  if (check) {
+  if (check || m == 1) {
     appleCount = pick_apples(m, fruits);
     return {insertedIndices, appleCount};
   }
 
-  for (int i = m-1; i < n; i += m) {
-    vector<vector<int>> V(m, vector<int>(2));
-    // Inicializacija
-    for (int j = 0; j < m; j++) {
-        V[j][0] = 0;
-    }
+  while (!stop) {
+    stop = true;
+    for (int i = m-1; i < n && k != 0; i += m) {
+      int value1 = 0;
+      int value2 = 0;
 
-    int j = i;
-    int l = 0;
+      if (fruits[i] == 1 && fruits[i-1] == 0) {
+        for (int j = i; j < n; j += m) {
+          value1 += fruits[j];
+        }
+        for (int j = i-1; j < n; j += m) {
+          value2 += fruits[j];
+        }
 
-    while (j >= i-m+1 || l < k) {
-      for (int p = j; p < n; p += m) {
-        V[l][0] += fruits[p];
-      }
-      V[l][1] = j;
-      l++;
-      j--;
-    }
-
-    int best_value = V[0][0];
-    int best_index = V[0][1];
-    int cost = 0;
-
-    for (int x = 1; x < m; x++) {
-      if (V[x][0] < best_value) {
-        best_value = V[x][0];
-        best_index = V[x][1];
-        cost = x;
+        if (value2 <= value1) {
+          fruits.insert(fruits.begin() + i-1, 1);
+          insertedIndices.emplace_back(i);
+          stop = false;
+          k--;
+        }
       }
     }
-
-    fruits.insert(fruits.begin() + best_index, 1);
-    insertedIndices.emplace_back(best_index+1);
-    k -= cost;
   }
-
-  for (int f : fruits) {
-      cout << f;
-  }
-  cout << endl;
 
   appleCount = pick_apples(m, fruits);
 
@@ -141,7 +136,7 @@ int main(int argc, char *argv[]) {
   file.close();
 
   cout << "n: " << n << ", k: " << k << ", m: " << m << endl;
-  cout << fruit << endl;
+  //cout << fruit << endl;
 
   vector<bool> fruits = stringToVector(fruit);
   pair<vector<int>, int> result = solve(n, k, m, fruits);
